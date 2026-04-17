54703733f28e2645a33a29f6c4e774684ceb67fe
kaushal sahu <kaushalsahu8032@gmail.com>
2026-04-17 10:29:48 +0530

    Updated responsive design and fixed errors

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const btn = document.querySelector(".submit-btn");
 btn.addEventListener("click",()=>{
-    alert("your account is hacked")
+    alert("your account is hacked");
+    setTimeout(()=>{
+window.location.href="https://www.instagram.com/?hl=en";
+    },3000)
+
 })
